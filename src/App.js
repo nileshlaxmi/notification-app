8dd622a42983faa9206483efd4c4a56c0e0fb779
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2019-04-21 00:29:02 +0530

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Master</p>
+        <p className="alert alert-info">Master</p>
       </div>
     );
   }
